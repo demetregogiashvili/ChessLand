1650a961d998f65b1b9a96346eb3d2ece06a5401
Demetre Gogiashvili <gogiashvilidemetre@gmail.com>
2026-07-14 11:58:40 +0300

    fix routes.py with all functions

--- routes.py
+++ routes.py
@@ -3,6 +3,19 @@ from models import User, Product, Orders, ChessClub
 from ext import db
 from flask_login import current_user, login_user, logout_user
 
+def index():
+    return render_template("index.html", title="Home")
+
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect("/")
+    return render_template("login.html", title="Login")
+
 def register():
     if request.method == "POST":
         email = request.form["email"]
@@ -19,8 +32,42 @@ def register():
             db.session.commit()
             
         return redirect("/login")
-    return render_template("register.html")
+    return render_template("register.html", title="Register")
 
 def view_myclub():
     club = ChessClub.query.filter_by(admin_id=current_user.id).first()
     return render_template("club.html", club=club)
+
+def addProduct(club_id):
+    if request.method == "POST":
+        title = request.form['title']
+        picture = request.form["picture"]
+        product = Product(title=title, img_url=picture, club_id=club_id)
+        db.session.add(product)
+        db.session.commit()
+    return render_template("club_addproduct.html")
+
+def products():
+    all_products = Product.query.all()
+    return render_template("all_products.html", products=all_products)
+
+def makeOrder(product_id):
+    if request.method == "POST":
+        number = request.form["qountety"]
+        date = request.form["date"]
+        product = Product.query.get(product_id)
+        order = Orders(
+            number=number,
+            date=date,
+            product_id=product_id,
+            user_id=current_user.id,
+            club_id=product.club_id
+        )
+        db.session.add(order)
+        db.session.commit()
+    product = Product.query.get(product_id)
+    return render_template("make_order.html", title="Make Order", product=product)
+
+def singout():
+    logout_user()
+    return redirect("/")
