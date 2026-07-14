ca896b86f2402ada7bdfb5d1b1589df66a0e8d68
Demetre Gogiashvili <gogiashvilidemetre@gmail.com>
2026-07-14 18:49:27 +0300

    My own project version

--- models.py
+++ models.py
@@ -1,35 +1,47 @@
 from ext import db
 from flask_login import UserMixin
-
-class User(db.Model, UserMixin): # აქ გაქვს ყველაფერი ერთად
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    name = db.Column(db.String)
-    surname = db.Column(db.String)
-    email = db.Column(db.String(150), unique=True)
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String,unique = True,nullable = False)
+    password = db.Column(db.String,nullable = False)
+    name = db.Column(db.String,nullable = False)
+    surname = db.Column(db.String,nullable = False)
+    email = db.Column(db.Integer,unique = True)
     profile_url = db.Column(db.String)
-    is_admin = db.Column(db.Boolean, default=False)
-
-class ChessClub(db.Model):
-    __tablename__ = "chess_clubs"
-    id = db.Column(db.Integer, primary_key=True)
-    club_name = db.Column(db.String)
-    admin_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    products = db.relationship("Product", backref="club")
-
+    is_admin = db.Column(db.Boolean,default = False)
+    is_admin = db.relationship(
+        "Admins",
+        backref = "Admin"
+    )
+    orders = db.relationship(
+        "Orders",
+        backref = "user"
+    )
+class Admin(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    Adminname = db.Column(db.String)
+    Admin_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+    products = db.relationship(
+        "Product",
+        backref = "Admin"
+    )
+    orders = db.relationship(
+        "Orders",
+        backref = "Admin"
+    )
 class Product(db.Model):
-    __tablename__ = "product"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer,primary_key= True)
     title = db.Column(db.String)
     img_url = db.Column(db.String)
-    club_id = db.Column(db.Integer, db.ForeignKey("chess_clubs.id"))
-
+    farm_id = db.Column(db.Integer,db.ForeignKey("farms.id"))
+    orders = db.relationship(
+        "Orders",
+        backref = "product"
+    )
 class Orders(db.Model):
-    __tablename__ = "orders"
-    id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String)
+    id = db.Column(db.Integer,primary_key = True)
+    number = db.Column(db.Integer)
     date = db.Column(db.String)
-    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    club_id = db.Column(db.Integer, db.ForeignKey("chess_clubs.id"))
+    product_id = db.Column(db.Integer,db.ForeignKey("product.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+    farm_id = db.Column(db.Integer,db.ForeignKey("farms.id"))
--- routes.py
+++ routes.py
@@ -1,48 +1,83 @@
-from flask import render_template, request, redirect 
-from ext import db 
-from models import User, ChessClub 
+from flask import render_template,request,redirect
+from models import User,Farms,Product,Orders
+from ext import db, login_manager
+from flask_login import current_user,login_user,logout_user,login_required
+def index():
+    return render_template("index.html",title = "home")
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if user.password == password:
+                login_user(user)
+                return redirect("/")
+            return redirect("/login")
+        return redirect("/register")
+    return render_template("login.html",title = "login")
 def register():
-
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
         username = request.form["username"]
+        name = request.form["name"]
+        surname = request.form["lastname"]
+        img_url = request.form["img_url"]
+        try:
+            Admin = request.form["Admin"]
+        except:
+            isAdmin = "noAdmin"
+        if User.query.filter_by(email = email).first():
+            return render_template("register.html",title = "register",message = "Email already used")
+        if User.query.filter_by(username = username).first():
+            return render_template("register.html",title = "register",message = "username already used")
 
-        # ვამოწმებთ, არსებობს თუ არა უკვე ეს მეილი
-        existing_user = User.query.filter_by(email=email).first()
-        if existing_user:
-            return "ეს მეილი უკვე დარეგისტრირებულია! სცადე სხვა."
-
-        # თუ არ არსებობს, ვქმნით ახალ მომხმარებელს
-        user = User(username=username, password=password, email=email, name=username)
+        user = User(
+            username = username,password = password,name = name,
+            surname = surname,email = email,profile_url = img_url
+        )
         db.session.add(user)
         db.session.commit()
-
-        if request.form.get("isAdmin") == "admin":
-            club = ChessClub(club_name=username, admin_id=user.id)
+        if isAdmin == "Admin":
+            user.is_admin = True
+            club= club(clubmname = username,club_id = club.id)
             db.session.add(club)
             db.session.commit()
-            
         return redirect("/login")
-    return render_template("register.html", title="Register")
-
-def index():
-    return render_template("index.html")
-
-def login():
-    return render_template("login.html")
-
-def view_myclub():
-    return render_template("club.html")
-
-def addProduct():
-    return render_template("make_order.html") # ან შენი შესაბამისი გვერდი
-
+    return render_template("register.html",title = "register")
+def club():
+    club = club.query.filter_by(Admin_id = current_user.id).first()
+    print(club.clubname)
+    return render_template("land.html",club = club)
+def addProduct(club_id):
+    if request.method == "POST":
+        title = request.form['title']
+        picture = request.form["picture"]
+        product = Product(title = title , img_url = picture , club_id = club_id)
+        db.session.add(product)
+        db.session.commit()
+    return render_template("land_addproduct.html")
 def products():
-    return render_template("product_card.html")
-
-def makeOrder():
-    return render_template("make_order.html")
-
+    products = Product.query.all()
+    return render_template("all_products.html",products = products)
+def makeOrder(product_id):
+    if request.method == "POST":
+        number = request.form["qountety"]
+        date = request.form["date"]
+        product = Product.query.get(product_id)
+        order = Orders(
+            number = number,
+            date = date,
+            product_id = product_id,
+            user_id = current_user.id,
+            farm_id = product.farm.id
+        )
+        db.session.add(order)
+        db.session.commit()
+    product = Product.query.get(product_id)
+    return render_template("make_order.html",title = "make order",product = product)
+@login_required
 def singout():
+    logout_user()
     return redirect("/")
